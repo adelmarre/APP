631a2dc9058a6569adb60438d75e4b9e17df822d
Doubidouchet <45229609+Doubidouchet@users.noreply.github.com>
2018-11-25 15:12:36 +0100

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -1,16 +1,16 @@
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre','root','');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root','');
 	if(isset($_GET['mail'],$_GET['key'])  AND ! empty($_GET['mail']) AND !empty($_GET['key'])){
 		$mail= htmlspecialchars(urldecode($_GET['mail']));
 		$key = htmlspecialchars($_GET['key']);
-		$requser = $bdd -> prepare("SELECT * FROM membres WHERE mail = ? AND confirmkey = ? ");
+		$requser = $bdd -> prepare("SELECT * FROM personne WHERE mail = ? AND confirmkey = ? ");
 		$requser -> execute(array($mail,$key));
 		$userexist = $requser -> rowCount();
 
 		if ($userexist == 1){
 			$user = $requser -> fetch();
 			if ($user ['confirme'] == 0 ){
-				$updateuser = $bdd -> prepare("UPDATE membres SET confirme = 1 WHERE mail= ? AND confirmkey = ? ");
+				$updateuser = $bdd -> prepare("UPDATE personne SET confirme = 1 WHERE mail= ? AND confirmkey = ? ");
 				$updateuser -> execute(array($mail,$key));
 				echo "Votre inscription a bien été confirmée !";
 
