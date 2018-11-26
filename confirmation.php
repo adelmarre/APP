f8533c77a2f568191eac47b2b402c6060bfd399a
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-11-26 22:56:46 +0100

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -1,27 +1,74 @@
 <?php
+
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root','');
-	if(isset($_GET['mail'],$_GET['key'])  AND ! empty($_GET['mail']) AND !empty($_GET['key'])){
+
+	if(isset($_GET['mail'],$_GET['key'])  AND !empty($_GET['mail']) AND !empty($_GET['key'])){
+
 		$mail= htmlspecialchars(urldecode($_GET['mail']));
+
 		$key = htmlspecialchars($_GET['key']);
+
 		$requser = $bdd -> prepare("SELECT * FROM personne WHERE mail = ? AND confirmkey = ? ");
+
 		$requser -> execute(array($mail,$key));
+
 		$userexist = $requser -> rowCount();
 
+
+
 		if ($userexist == 1){
+
 			$user = $requser -> fetch();
+
 			if ($user ['confirme'] == 0 ){
+
 				$updateuser = $bdd -> prepare("UPDATE personne SET confirme = 1 WHERE mail= ? AND confirmkey = ? ");
+
 				$updateuser -> execute(array($mail,$key));
-				echo "Votre inscription a bien été confirmée !";
+
+
+				$erreur= "Votre inscription a bien été confirmée !";
+
+
 
 			}else {
-				echo "Votre inscription a déjà été confirmé";
+
+				$erreur= "Votre inscription a déjà été confirmé";
+
 			}
 
+
+
 		}else{
-			echo "L'utilisateur n'existe pas !";
-		}
+			
+
+			$erreur =  "L'utilisateur n'existe pas !";
 
 		}
+
+
+
+		}
+
+
 
 ?>
+
+<html>
+<head>
+        <meta charset="UTF-8" />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="inscription.css" />
+        <title>Confirmation de compte</title>
+    </head>
+
+<img src="logo hexagon final.png" alt="photo de hexagon" id="hexagon">
+<body><h1>  <?php
+            if (isset($erreur)) 
+            { 
+                echo $erreur;
+            }
+?></h1>
+
+<a href="index.php">Connexion</a> 
+</body>
