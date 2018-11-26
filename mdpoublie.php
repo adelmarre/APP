72ff4e5055d85fe2ead9cef8ff4a0427c1de11ba
Doubidouchet <45229609+Doubidouchet@users.noreply.github.com>
2018-11-26 19:18:07 +0100

    Update mdpoublie.php

--- mdpoublie.php
+++ mdpoublie.php
@@ -8,86 +8,83 @@ if(isset($_GET['section'])){
 }
 
 if (isset($_POST['recup_submit'], $_POST['recup_mail'])) {
-		if (!empty($_POST['recup_mail'])) {
-			$recup_mail = htmlspecialchars($_POST['recup_mail']);
-			if (filter_var($recup_mail,FILTER_VALIDATE_EMAIL)){
-				$mailexist = $bdd->prepare('SELECT id,prenom FROM personne WHERE mail = ?');
-				$mailexist->execute(array($recup_mail));
-				$mailexist_count = $mailexist->rowCount();
-				if ($mailexist_count == 1){
-					$prenom = $mailexist->fetch();
-					$prenom = $prenom['prenom'];
 
-					$_SESSION['recup_mail'] = $recup_mail;
-					$recup_code = "";
-					for ($i=0; $i < 8 ; $i++) {
-						$recup_code .= mt_rand(0,9);
+	if (!empty($_POST['recup_mail'])) {
+		$recup_mail = htmlspecialchars($_POST['recup_mail']);
+		if (filter_var($recup_mail,FILTER_VALIDATE_EMAIL)){
+			$mailexist = $bdd->prepare('SELECT id,prenom FROM personne WHERE mail = ?');
+			$mailexist->execute(array($recup_mail));
+			$mailexist_count = $mailexist->rowCount();
+			if ($mailexist_count == 1){
+				$prenom = $mailexist->fetch();
+				$prenom = $prenom['prenom'];
 
-					}
-					$_SESSION['recup_code'] = $recup_code;
-
-					$mail_recup_exist = $bdd->prepare('SELECT id FROM recuperation WHERE mail =?');
-					$mail_recup_exist->execute(array($recup_mail));
-					$mail_recup_exist = $mail_recup_exist->rowCount();
-
-					if ($mail_recup_exist == 1) {
-						$recup_insert = $bdd->prepare('UPDATE recuperation SET code = ? WHERE mail = ?');
-						$recup_insert->execute(array($recup_code,$recup_mail));
-					}else{
-						$recup_insert = $bdd->prepare('INSERT INTO recuperation(mail,code) VALUES (?,?)');
-						$recup_insert->execute(array($recup_mail,$recup_code));
-					}
-					$header="MIME-Version: 1.0\r\n";
-			        $header.='From:"Hexagon.com"<projet.hexagon@gmail.com>'."\n";
-			        $header.='Content-Type:text/html; charset="utf-8"'."\n";
-			        $header.='Content-Transfer-Encoding: 8bit';
-			        $message = '
-			        <html>
-			        <head>
-			          <title>Récupération de mot de passe - Hexagon.com</title>
-			          <meta charset="utf-8" />
-			        </head>
-			        <body>
-			          <font color="#303030";>
-			            <div align="center">
-			              <table width="600px">
-			                <tr>
-			                  <td>
-			                     
-			                    <div align="center">Bonjour <b>'.$prenom.'</b>,</div>
-			                    Voici votre code de récupération: <b>'.$recup_code.'</b>
-			                     
-			                  </td>
-			                </tr>
-			                <tr>
-			                  <td align="center">
-			                    <font size="2">
-			                      Ceci est un email automatique, merci de ne pas y répondre
-			                    </font>
-			                  </td>
-			                </tr>
-			              </table>
-			            </div>
-			          </font>
-			        </body>
-			        </html>
-			        ';
-			        mail($recup_mail, "Récupération de mot de passe - Hexagon.com", $message, $header);
-			        header("Location:http://127.0.0.1/Hexagon/mdp_oublie.php?section=code");
-
-					
-				}else{
-					$error = "Cette adresse mail n'est pas enregistrée";
+				$_SESSION['recup_mail'] = $recup_mail;
+				$recup_code = "";
+				for ($i=0; $i < 8 ; $i++) {
+					$recup_code .= mt_rand(0,9);
 				}
+				$_SESSION['recup_code'] = $recup_code;
+				$mail_recup_exist = $bdd->prepare('SELECT id FROM recuperation WHERE mail =?');
+				$mail_recup_exist->execute(array($recup_mail));
+				$mail_recup_exist = $mail_recup_exist->rowCount();
 
-
+				if ($mail_recup_exist == 1) {
+					$recup_insert = $bdd->prepare('UPDATE recuperation SET code = ? WHERE mail = ?');
+					$recup_insert->execute(array($recup_code,$recup_mail));
+				}else{
+					$recup_insert = $bdd->prepare('INSERT INTO recuperation(mail,code) VALUES (?,?)');
+					$recup_insert->execute(array($recup_mail,$recup_code));
+				}
+				$header="MIME-Version: 1.0\r\n";
+		        $header.='From:"Hexagon.com"<projet.hexagon@gmail.com>'."\n";
+		        $header.='Content-Type:text/html; charset="utf-8"'."\n";
+		        $header.='Content-Transfer-Encoding: 8bit';
+		        $message = '
+		        <html>
+		        <head>
+		          <title>Récupération de mot de passe - Hexagon.com</title>
+		          <meta charset="utf-8" />
+		        </head>
+		        <body>
+		          <font color="#303030";>
+		            <div align="center">
+		              <table width="600px">
+		                <tr>
+		                  <td>
+		                     
+		                    <div align="center">Bonjour <b>'.$prenom.'</b>,</div>
+		                    Voici votre code de récupération: <b>'.$recup_code.'</b>
+		                     
+		                  </td>
+		                </tr>
+		                <tr>
+		                  <td align="center">
+		                    <font size="2">
+		                      Ceci est un email automatique, merci de ne pas y répondre
+		                    </font>
+		                  </td>
+		                </tr>
+		              </table>
+		            </div>
+		          </font>
+		        </body>
+		        </html>
+		        ';
+		        mail($recup_mail, "Récupération de mot de passe - Hexagon.com", $message, $header);
+		        header("Location:http://localhost/Hexagon/mdp_oublie.php?section=code");
+					
 			}else{
-				$error = "Adresse mail incorrect";
+				$error = "Cette adresse mail n'est pas enregistrée";
 			}
+
 		}else{
-			$error = "Veuillez entrer votre adresse mail";
+			$error = "Adresse mail incorrect";
 		}
+	}else{
+		$error = "Veuillez entrer votre adresse mail";
 	}
+}
 	if (isset($_POST['verif_submit'],$_POST['verif_code'])){
 		if(!empty($_POST['verif_code'])){
 			$verif_code = htmlspecialchars($_POST['verif_code']);
@@ -97,7 +94,7 @@ if (isset($_POST['recup_submit'], $_POST['recup_mail'])) {
 			if ($verif_req == 1) {
 				$del_req = $bdd->prepare('DELETE FROM recuperation WHERE mail = ?');
 				$del_req->execute (array($_SESSION['recup_mail']));
-				header("Location:http://127.0.0.1/Hexagon/mdp_oublie.php?section=changemdp");
+				header("Location:http://localhost/Hexagon/mdp_oublie.php?section=changemdp");
 			}else{
 				$error = "Code invalide";
 			}
@@ -106,6 +103,7 @@ if (isset($_POST['recup_submit'], $_POST['recup_mail'])) {
 			$error = "Veuillez entrer votre code de vérification";
 		}
 	}
+
 	if (isset($_POST['change_submit'],$_POST['change_mdp'],$_POST['change_mdpc'])){
 		$mdp = htmlspecialchars($_POST['change_mdp']);
 		$mdpc = htmlspecialchars($_POST['change_mdpc']);
@@ -114,7 +112,7 @@ if (isset($_POST['recup_submit'], $_POST['recup_mail'])) {
 				$mdp = sha1($mdp);
 				$ins_mdp = $bdd->prepare('UPDATE personne SET mdp = ? WHERE mail = ?');
 				$ins_mdp->execute(array($mdp,$_SESSION['recup_mail']));
-				header('Location:http://127.0.0.1/Hexagon/');
+				header('Location:http://localhost/Hexagon/');
 				
 			}else{
 				$error = "Vos mots de passes ne correspondent pas";
