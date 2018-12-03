6c0f806b954685ba04edd528feb598efa3f8d76d
adelmarre <45226794+adelmarre@users.noreply.github.com>
2018-12-03 17:26:25 +0100

    Add files via upload

---
+++ conditions.php
@@ -0,0 +1,101 @@
+
+<html>
+<head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="inscription.css" />
+        <title>Conditions générales d'utilisations</title>
+    </head>
+<body>
+    
+     
+    <h1>Conditions générales d'utilisation</h1><form method="POST" action="">
+  <div>
+    Article 1 : Objet
+  </div>
+ <p>Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique des modalités de mise à disposition des services du site Hexagon et leur utilisation par « l'Utilisateur ».</br>
+Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur souhaitant accéder au site. Elles constituent le contrat entre le site et l'Utilisateur. L’accès au site par l’Utilisateur signifie son acceptation des présentes conditions générales d’utilisation.</br>
+Eventuellement :<ul>
+ <li>En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.</li>
+<li>Hexagon se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.</li></p></ul>
+  
+    <div>Article 2 : Mentions Légales</div>
+<p>L'édition du site Hexagon est assurée par la Société <font color="red">[Nom de la société] [SAS / SA / SARL, etc.]</font> au capital de <font color="red">[montant en euros]</font> € dont le siège social est situé au 10 rue de vanves, 92130, Issy-les-moulineaux.</br>
+La Directrice de la publication est Madame Hashley RAMANANKATSOINA</p>
+<div>Article 3 : Définitions</div>
+<p>La présente clause a pour objet de définir les différents termes essentiels du contrat :
+<ul><li>Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés par le site.</li>
+<li>Contenu utilisateur : ce sont les données transmises par l'Utilisateur au sein du site.</li>
+<li>Membre : l'Utilisateur devient membre lorsqu'il est identifié sur le site.</li>
+<li>Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un Utilisateur sur le site. L'identifiant et le mot de passe permettent à l'Utilisateur d'accéder à des services réservés aux membres du site. Le mot de passe est confidentiel.</li></ul></p>
+  <div>Article 4 : Définitions</div>
+  <p> Le site est accessible à tout Utilisateur ayant un accès à Internet et ayant un forfait chez Domisep (voire offres Domisep pour plus d'informations). Les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</br>
+
+  Selon le cas :</br>
+
+  L’Utilisateur non membre n'a pas accès aux services réservés aux membres. Pour cela, il doit s'identifier à l'aide de son identifiant et de son mot de passe et un forfait domotique chez Domisep. </br>
+
+  Le site met en œuvre tous les moyens mis à sa disposition pour assurer un accès de qualité à ses services. L'obligation étant de moyens, le site ne s'engage pas à atteindre ce résultat.</br>
+
+  Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du réseau ou du serveur sera signalé.</br>
+
+  L'accès aux services du site peut à tout moment faire l'objet d'une interruption, d'une suspension, d'une modification sans préavis pour une maintenance ou pour tout autre cas. L'Utilisateur s'oblige à ne réclamer aucune indemnisation suite à l'interruption, à la suspension ou à la modification du présent contrat.</br>
+
+  L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse <font color="red">contact@nomdusite.com]</font>. </p>
+  <div>Article 5 : Propriété intellectuelle</div>
+  <p>Les marques, logos, signes et tout autre contenu du site font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</br>
+
+L'Utilisateur sollicite l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.</br>
+
+L'Utilisateur s'engage à une utilisation des contenus du site dans un cadre strictement privé. Une utilisation des contenus à des fins commerciales est strictement interdite.</br>
+
+Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur. </p>
+<div>Article 6 : Données personnelles</div>
+<p>
+ Les informations demandées à l’inscription au site sont nécessaires et obligatoires pour la création du compte de l'Utilisateur. 
+
+Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. 
+
+En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+<ul><li>
+    son espace personnel ;</li>
+
+   <li> un formulaire de contact ;</li>
+
+       <li> par mail à <font color="red">[adresse mail]</font> ;</li>
+
+
+       <li> par voie postale au<font color="red">[adresse]</font>.</li></ul>
+</p>
+<div>ARTICLE 7 : Responsabilité et force majeure</div>
+ <p>Les sources des informations diffusées sur le site sont réputées fiables.
+
+L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite.
+
+L'Utilisateur assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+
+Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+Toutefois, le site s'engage à mettre en œuvre tous les moyens nécessaires afin de garantir au mieux la sécurité et la confidentialité des données.
+
+La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
+
+<div>ARTICLE 8 : Liens hypertextes</div>
+<p> Les liens hypertexte mèneront vers des sites gérés par Domisep.</p>
+<div>ARTICLE 9 : Evolution du contrat </div>
+
+<p>Le site se réserve à tout moment le droit de modifier les clauses stipulées dans le présent contrat. L'utilisateur sera prévenue dans ce cas et devra accepter de nouvelles conditions d'utilisation.</p>
+
+<div>ARTICLE 10 : Durée</div>
+<p>La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter de l'utilisation du service.</p>
+
+<div>ARTICLE 11 : Droit applicable et juridiction compétente</div>
+<p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, seuls les tribunaux <font color="red">[du ressort de la Cour d'appel de / de la ville de] [Ville]</font> sont compétents.</p>
+  <div>ARTICLE 12 : Publication par l’Utilisateur</div>
+<p> Le site permet aux membres de publier [des commentaires / des œuvres personnelles].
+Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle.</p>
+ 
+<div> <a href="inscription.php">Retour</a></div>
+
+
+
+</form>
+</body>
