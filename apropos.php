9cb4d2b3fc667d18b574da9e361a11dd22b05d8a
adelmarre <45226794+adelmarre@users.noreply.github.com>
2018-12-07 14:39:49 +0100

    Add files via upload

---
+++ apropos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="apropos.css">
+		<meta charset="utf-8">
+		<title>A propos</title>
+	</head>
+		<?php require "header.html"; ?>
+	<body>
+		<div class="blocpage">
+		<h1>A propos</h1>
+
+
+			<div class="contenu">
+					
+						<img src="logo dominium.png" alt="Photo de dominium" id="dominium" class="logo_dominium">
+
+						<div class="texte">
+							<p>Hexagon est un projet répondant à la demande de Domisep faite à la start-up Dominium (spécialisée en informatique, télécommunications, électronique et traitement du signal) de créer une plate-forme web afin de pouvoir gérer des habitations connectées de particuliers à distance. </br></br>
+
+							Grâce à cette plateforme il vous est possible de gérer les équipements domotiques (radiateurs, climatiseurs, volets roulants, alarmes…) de votre résidence principale (et de vos habitations secondaires) en un clic sans déplacement ! Que ce soit pour régler le chauffage, éteindre une lumière oubliée ou sécuriser votre maison … toutes ces actions vous sont rendues possibles et faciles via votre compte. Il vous est même possible de permettre l’accès à votre espace "Ma maison" avec des fonctionnalités spécifiques à d’autres personnes (membres de votre famille…) en toute simplicité. De plus, notre entreprise vous propose un large choix d'équipement afin de répondre au mieux à votre demande.</br></br>
+
+							Vous êtes intéressé par notre installation? Vous avez des questions ? N’hésitez pas à nous contacter. 
+							</p>
+						</div>
+					
+			
+			</div>
+			<?php require "footer.html"; ?>
+		</div>
+	
+	</body>
+	
+</html>
