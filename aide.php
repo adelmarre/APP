13535df9d70f94f6c77a7cc3c06b638cfabe9700
adelmarre <45226794+adelmarre@users.noreply.github.com>
2018-12-10 15:16:04 +0100

    Add files via upload

---
+++ aide.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html">
+<?php
+
+include 'header.php';
+
+?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="general.css">
+  	<meta charset="utf-8"/>
+  	<title>Aide</title>
+  	<link rel="stylesheet" href="aide.css">
+  </head>
+
+  <body>
+    
+    <div class="banniere">
+        <img src="image_faq.jpg" alt="image_faq" id="image_faq" width="1235" height="250">
+    </div>
+
+    <div class="ensemble">
+      <h1>Questions les plus consultées</h1>
+
+      <div id="colonne1"> 
+        <h2>   <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+        Comment se déroule l’installation ? Est-il nécessaire de faire des travaux importants ?</h2>
+      <div class="questions">
+        <p><h5>L’installation ne nécessite pas de travaux. Elle se déroule sur une journée : un technicien se rend dans votre habitation afin d’y placer les capteurs, les passerelles intermédiaires et la passerelle centrale.</h5></p>
+      </div>
+      <h2> <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+      Comment puis-je effectuer des modifications concernant ma maison (exemple : ajout de fonctionnalité) ?</h2>
+      <div class="questions">
+        <p>A partir de votre espace Ma maison, vous pouvez ajouter des capteurs et des salles facilement.</p>
+      </div>
+    </div>
+
+    <div id="colonne2">
+      <h2> <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+        Quelles fonctions puis-je contrôler depuis mon compte sur le site web ?</h2>
+      <div class="questions">
+        <p>Depuis votre page Ma maison, il vous est possible d’accéder, pour chaque pièce de votre habitation, aux informations des capteurs (température, luminosité... ) ainsi qu’à l’historique des données. A partir de ces informations, vous pouvez décider de modifier la température de votre habitation, d'éteindre une lampe… </p>
+      </div>
+      <h2> <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+        Que se passe-t-il en cas de panne ?</h2>
+      <div class="questions">
+        <p>En cas de panne, nous en sommes avertis et un technicien interviendra dans votre habitation pour résoudre le problème dans les plus bref délais.</p>
+
+      </div>
+    </div>
+
+
+  <div id="centre">
+	 <h2> <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+      Peut-on permettre l’accès à notre domicile sur le site web à d’autres utilisateurs ?</h2>
+    <div class="questions">
+    	 <p>Il vous est possible de créer des accès à votre maison pour des utilisateurs secondaires avec la possibilité d’effectuer des actions différentes des vôtres.</p>
+    </div>
+    <h2> <img src="point_interrogation.jpg" alt="jk" style="width:20px;height:20px;">
+      Comment les installations fonctionnent-elles ?</h2>
+    <div class="questions">
+      <p>Lorsque vous voulez contrôler votre installation à distance, tous les composants domotiques sont reliés au serveur. Les données sont envoyées à ce serveur et il réalisera l'action à l'instant même dans votre habitation.</p>
+    </div>
+  </div>
+
+  </div>
+</body>
+</html>
+<?php 
+
+include 'footer.php'
+
+?>
