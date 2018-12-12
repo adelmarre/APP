2fe7426d869c9440b05b94fc17709abe5a84c122
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-12 11:44:27 +0100

    Update faqversionfinale.php

--- faqversionfinale.php
+++ faqversionfinale.php
@@ -1,27 +1,34 @@
 <?php
-include 'header.php';
-?>
+session_start();
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root',''); ?>
 <html>
   <head>
   	<meta charset="utf-8"/>
   	<title>Aide</title>
   	<link rel="stylesheet" href="faqversionfinale.css">
+    <link rel="stylesheet" type="text/css" href="general.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
 </head>
 
 
 </html>
 
 <html>
-<head>
-</head>
+
+<?php if (isset($_SESSION['id'])) {
+  include "header_deco.php"; }
+  else {
+    include "header_connexion.php";
+  }
+?>
 <body>
 </br>
 <div class="ensemble">
-  <h1 style="font-family:comic sans ms;3>" align = 'center'>Questions les plus consultées</h1>
+  <h1 align = 'center'>Questions les plus consultées</h1>
 
   <div class = "row">
     <div id="colonne1"> 
-      <h5>   <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+      <h5>   <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
        Comment se déroule l’installation ? Est-il nécessaire de faire des travaux importants ?</h5>
       <div class="questions">
         <p> <h5>L’installation ne nécessite pas de travaux. Elle se déroule sur une journée : un technicien se rend dans votre habitation afin d’y placer les capteurs, les passerelles intermédiaires et la passerelle centrale.</h5></p>
@@ -30,7 +37,7 @@ include 'header.php';
     </div>
 
     <div id="colonne2">
-      <h5> <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+      <h5> <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
         Comment puis-je effectuer des modifications concernant ma maison (exemple : ajout de fonctionnalité) ?</h5>
       <div class="questions">
         <p><h5>A partir de votre espace Ma maison, vous pouvez ajouter des capteurs et des salles facilement.</h5></p>
@@ -39,7 +46,7 @@ include 'header.php';
 
 
     <div id="centre">
-    	<h5> <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+    	<h5> <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
         Quelles fonctions puis-je contrôler depuis mon compte sur le site web ?</h5>
       <div class="questions">
         	<p><h5>Depuis votre page Ma maison, il vous est possible d’accéder, pour chaque pièce de votre habitation, aux informations des capteurs (température, luminosité... ) ainsi qu’à l’historique des données. A partir de ces informations, vous pouvez décider de modifier la température de votre habitation, d'éteindre une lampe… </h5></p>
@@ -50,14 +57,14 @@ include 'header.php';
 
   <div class="row">
     <div id="colonne1">
-    <h5> <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+    <h5> <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
         Que se passe-t-il en cas de panne ?</h5>
       <div class="questions">
         <p><h5>En cas de panne, nous en sommes avertis et un technicien interviendra dans votre habitation pour résoudre le problème dans les plus bref délais.</h5></p>
       </div>
     </div>
     <div id="colonne2">
-      <h5> <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+      <h5> <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
         Peut-on permettre l’accès à notre domicile sur le site web à d’autres utilisateurs ?</h5>
       <div class="questions">
         <p><h5>Il vous est possible de créer des accès à votre maison pour des utilisateurs secondaires avec la possibilité d’effectuer des actions différentes des vôtres.</h5></p>
@@ -65,7 +72,7 @@ include 'header.php';
     </div>
     <div id="centre">
     </br>
-      <h5> <img src="point_interrogation.jpg" alt="jk" style="width:6.5%">
+      <h5> <img src="image/point_interrogation.jpg" alt="jk" style="width:6.5%">
         Comment les installations fonctionnent-elles ?</h5>
       <div class="questions">
         <p><h5>Lorsque vous voulez contrôler votre installation à distance, tous les composants domotiques sont reliés au serveur. Les données sont envoyées à ce serveur et il réalisera l'action à l'instant même dans votre habitation.</h5></p>
