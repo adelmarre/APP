8d139ca923f74ba1eb3faae51730a8bf22fdd472
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-17 17:38:32 +0100

    Update gerermamaison.php

--- gerermamaison.php
+++ gerermamaison.php
@@ -1,31 +1,47 @@
+
+
 <?php 
 session_start();
 try {
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root','');
-if (isset($_SESSION['id']))
-{
-  $getid = intval($_SESSION['id']);
-  $requser = $bdd -> prepare('SELECT * FROM personne WHERE id = ?');
-  $requser -> execute(array($getid));
-  $userinfo = $requser ->fetch();
+if (isset($_SESSION['id'])) 
+	{
+	  $getid = intval($_SESSION['id']);
+	  $requser = $bdd -> prepare('SELECT * FROM personne WHERE id = ?');
+	  $requser -> execute(array($getid));
+	  $userinfo = $requser ->fetch();
+	  if (isset($_GET['id_habitation'])) {
+	  	 $gethabitation = intval($_GET['id_habitation']);
+	  }
+	}
+else {
+	exit();
 }
 }
 catch(Exception $e){
   die('Erreur : ' . $e->getMessage());
 }
-if (isset($_GET['valide1']))
+
+$requser = $bdd -> prepare('SELECT * FROM habitation JOIN personne ON personne.id = habitation.id_personne WHERE (id = ?) AND (id_habitation = ?)');
+$requser -> execute(array($getid,$gethabitation));
+$user = $requser ->fetch();
+$requser2 = $bdd -> prepare('SELECT * FROM piece WHERE (id_habitation = ?)');
+$requser2 -> execute(array($gethabitation));
+
+
+if (isset($_POST['valide1']))
 {
-	$nommaison = htmlspecialchars($_GET['nommaison']);
-	$superficie = htmlspecialchars($_GET['superficie']);
-	$nbpiece = htmlspecialchars($_GET['nbpiece']);
-	$adresse = htmlspecialchars($_GET['adresse']);
-	$ville = htmlspecialchars($_GET['ville']);
-	$cp = htmlspecialchars($_GET['cp']);
-	$pays = htmlspecialchars($_GET['pays']);
+	$nommaison = htmlspecialchars($_POST['nommaison']);
+
+	
+	$adresse = htmlspecialchars($_POST['adresse']);
+	$ville = htmlspecialchars($_POST['ville']);
+	$cp = htmlspecialchars($_POST['cp']);
+	$pays = htmlspecialchars($_POST['pays']);
 	{	
 		try{
-		$insertmais = $bdd->prepare("INSERT INTO habitation(nomhabitation, superficie,  adresse,id_personne,ville,pays,cp,nbpiece) VALUES(?, ?, ?, ?,?,?,?,?)");
-    	$insertmais->execute(array($nommaison, $superficie, $adresse,$userinfo['id'],$ville,$pays,$cp,$nbpiece));
+		$insertmais = $bdd->prepare("INSERT INTO habitation(nomhabitation, adresse,id_personne,ville,pays,cp) VALUES(?, ?,?,?,?,?)");
+    	$insertmais->execute(array($nommaison, $adresse,$userinfo['id'],$ville,$pays,$cp));
     }
     catch(Exception $e){
     	die("erreur bdd");
@@ -41,74 +57,123 @@ if (isset($_GET['valide1']))
 
 <html>
 <head>
-
 	<title>Gérer ma Maison</title>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="gerermamaisonstyle.css">
 	<link rel="stylesheet" type="text/css" href="general.css">
 
 	<script type="text/javascript">
+
 		function AfficheFormulaire(){
 			
 			
 			
+
 			var AjHab1=document.getElementById('ajoutHab1');
 			var AjHab2=document.getElementById('ajoutHab2');
+
 			var AjM=document.getElementById('ajmais');
 			var UserSec=document.getElementById('modutilsecond');
+
+
 			AjHab1.style.display='none';
 			AjHab2.style.display='none';
 			var FieldUser=document.getElementById('utilSecond');
+
+
 			FieldUser.style.display='none';
+
 			
 			if(AjM.checked==true){
 				AjHab1.style.display='block';
 				AjHab2.style.display='block';
 			}
+
 			else if(UserSec.checked==true) {
 				FieldUser.style.display='block';
+
 				
 			}
+			var ajsal=document.getElementById('ajoutersalle');
+			var radioajsal =document.getElementById('ajsal');
+			ajsal.style.display='none';
+			if (radioajsal.checked==true) {
+				ajsal.style.display='block';
+			}
+
+			// pour modifier sa maison
+			var modmais=document.getElementById('modifiermaison');
+			var radiomodmais=document.getElementById('modmais');
+			modmais.style.display='none';
+			if (radiomodmais.checked==true) {
+				modmais.style.display='block';
+			}
 		}
+
 		function AfficheMail(){
 			var Mail=document.getElementById('mail');
+
 			var US1=document.getElementById('uss1');
 			var US2=document.getElementById('uss2');
+
 			Mail.style.display='none';
+
 			if(US1.checked==true  ||  US2.checked==true ){
+
 				Mail.style.display='block';
 			}
 		}
+
+
 		function AfficheTableau(){
+
 			var US1=document.getElementById('uss1');
 			var US2=document.getElementById('uss2');
+
 			var MP=document.getElementById('MP');
 			var SP=document.getElementById('SP');
+
 			var Mail=document.getElementById('mail');
+
 			var TMUS1=document.getElementById('tableauUS1MP');
+
 			
 			TMUS1.style.display='none';
+
+
 			
 			if(US1.checked==true  && MP.checked==true ){
 				AfficheMail()
 				TMUS1.style.display='table';
 				
 			}
+
+
 		}
+		function AfficheAjouterSalle()  {
+			var ajsal=document.getElementById('ajoutersalle');
+			var radioajsal =document.getElementById('ajsal');
+			ajsal.style.display='none';
+			if (radioajsal.checked==true) {
+				ajsal.style.display='block';
+			}
+		}
+
+
+
 		
+
 		
 	</script>
 
 </head>
 <body>
-
-
 	<?php include "header_none.php" ?>
 	  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
 
 	<div class="colonnedroite">
 		<div class="box_profil">
-			<img src="profil.jpg" class="photo_profil">
+			<img src="image/profil.jpg" class="photo_profil">
 			<br/>
 			<div class="affichage_prenom">
 				<?php echo $userinfo['prenom'];?>
@@ -119,7 +184,6 @@ if (isset($_GET['valide1']))
 		</div>
 
 		<nav>
-
     <ul class="box_information">
 
  
@@ -164,7 +228,7 @@ if (isset($_GET['valide1']))
 
   <li> 
 
-   <p> <?php echo '<a href="gerermamaison.php?id='.$_SESSION['id'].'" class="box_rouge">Gérer ma maison</a>'; ?> 
+   <p> <?php echo '<a href="gerermamaison.php?id='.$_SESSION['id'].'&id_habitation='.$_GET['id_habitation'].'" class="box_rouge">Gérer ma maison</a>'; ?> 
     
 
 
@@ -177,7 +241,7 @@ if (isset($_GET['valide1']))
 
    <li> 
 
-   <p> <?php echo '<a href="MaisonSalleCapteurTempo.php?id='.$_SESSION['id'].'" class="box_rouge">Ma Maison</a>'; ?> 
+   <p> <?php echo '<a href="MaisonSalleCapteur.php?id='.$_SESSION['id'].'&id_habitation='.$_GET['id_habitation'].'" class="box_rouge">Ma Maison</a>'; ?> 
     
 
 
@@ -242,17 +306,80 @@ if (isset($_GET['valide1']))
 				<fieldset id="set">
 					<legend> <h1>Gérer ma maison</h1></legend>
 
-					
+					<header>
 						<nav class="head2">
 							<ul>
 								<li> <input type="radio" name="Sélection" value="Ajouter une salle" class="Selec" id="ajsal" onchange="AfficheFormulaire()"> Ajouter une salle </li>
 								<li><input type="radio" name="Sélection" value="Modifier ma Maison" class="Selec" id="modmais" onchange="AfficheFormulaire()"> Modifier ma Maison </li>
 								<li> <input type="radio" name="Sélection" value="Ajouter ma Maison" class="Selec" id="ajmais" onchange="AfficheFormulaire()"> Ajouter ma Maison  </li>
 								<li><input type="radio" name="Sélection" value="Modifier utilisateur secondaire" class="Selec" id="modutilsecond" onchange="AfficheFormulaire()"> Modifier utilisateur secondaire </li>
-							</ul>
-							</nav>
 
-						<form method="GET" action=""> 
+							</nav>
+						</header>
+
+						<div id="ajoutersalle">
+							<?php
+							if (isset($_POST['ajouter'])) {
+								$nom = htmlspecialchars($_POST['nom']);
+								$superficie = htmlspecialchars($_POST['superficie']);
+								$habitation = $_GET['habitation'];
+								$insertpiece = $bdd->prepare("INSERT INTO piece(nom, id_habitation, superficie) VALUES(?, ?, ?)");
+	                            $insertpiece->execute(array($nom, $habitation , $superficie));
+	                            $req_augmenterNbpiece = $bdd->prepare("UPDATE habitation SET nbpiece = (nbpiece+1) WHERE id_habitation = ? ");
+								$req_augmenterNbpiece -> execute(array($user['id_habitation']));
+								$newsuperficie = $user['superficie']+$superficie;
+								$req_augmenterSuperficie = $bdd->prepare("UPDATE habitation SET superficie = ? WHERE id_habitation = ? ");
+								$req_augmenterSuperficie -> execute(array($newsuperficie,$user['id_habitation']));
+
+							}?>
+
+						<fieldset>
+						<legend><h2>Ajouter une salle :</h2></legend>
+						<FORM method="POST">
+								<h3>Nom de la maison</h3>
+								<?php echo $user['nomhabitation'];?>
+								<h3><label>Nom de la salle :</label></h3>
+								<input type="text" id="nom" name="nom" required>
+
+								<h3><label>Superficie en m^2 :</label></h3>
+								<input type="number" id="superficie" name="superficie" min="0" value="0" required>
+
+							<input type="submit" name="ajouter" id="ajouter" value="Ajouter">
+
+							</FORM>	
+							</fieldset>
+						</div>
+						<div id="modifiermaison">
+						<fieldset>
+						<legend><h2>Modifier ma maison :</h2></legend>
+					
+						<div class="colonnegauche">
+							<label> Nom de la  Maison: </label>
+							<input type="text" class="RentrerInfo" name="newnomhabitation" value="<?php echo $user['nomhabitation']?>" required minlength="2" maxlength="16" size="16">
+							<label> Superficie en m^2 : </label>
+							<?php echo $user['superficie']?>
+
+							<label> Nombre de pièce : </label>
+							<?php echo $user['nbpiece']?>
+						</div>
+						<div class="colonnedroite">
+					
+							<label> Type de pièce :  </label>
+								<?php 
+							
+								while($p=$requser2->fetch() ) 
+								{?> 
+									<li><?=$p['nom']?> </li>
+							    
+							        <?php }?>
+							 <label > Addresse : </label>
+							<input type="text" class="RentrerInfo" name="newadresse" value="<?php echo $user['adresse']?>" required minlength="2" size="10">   
+							<label > Ville : </label>
+							<input type="text" class="RentrerInfo" name="newville" value="<?php echo $user['ville']?>" required minlength="2" size="10">   
+						</div>
+						</fieldset>
+						</div>
+						<form method="POST" action=""> 
 
 								<div class="colonnegauche">
 
@@ -260,9 +387,6 @@ if (isset($_GET['valide1']))
 
 										<label for="nomdelamaison"> Nom de la  Maison: </label>
 										<input type="text" class="RentrerInfo" name="nommaison" required minlength="2" maxlength="16" size="16">
-
-										<label for="superficie"> Superficie en m°2: </label>
-										<input type="number" class="RentrerInfo" name="superficie" required min="0">
 
 										<label for="pays"> Pays : </label>
 										<input type="text" class="RentrerInfo" name="pays" required min="0">
@@ -287,11 +411,6 @@ if (isset($_GET['valide1']))
 										<label for="addresse"> Addresse: </label>
 										<input type="text" class="RentrerInfo" name="adresse" required minlength="2" size="10">
 
-										<label for="nbpiece"> Nombre de pièce: </label>
-										<input type="number" class="RentrerInfo" name="nbpiece" required min="0" >
-
-										
-
 										<input type="submit" value="Valider" class="envoyer" name="valide1">
 										<input type="submit" value="Annuler" class="envoyer" is="suppr">
 
@@ -301,45 +420,7 @@ if (isset($_GET['valide1']))
 								</div>
 							</form>
 
-							<form method="GET" action=""> 
-
-								<div class="colonnegauche">
-
-									<div id="ajoutHab1">
-
-										<label for="nomdelamaison"> Nom de la  Maison: </label>
-										<input type="text" class="RentrerInfo" name="nommaison" required minlength="2" maxlength="16" size="16">
-
-										<label for="superficie"> Superficie en m°2: </label>
-										<input type="number" class="RentrerInfo" name="superficie" required min="0">
-
-										<label for="nbhab"> Nombre d'habitants: </label>
-										<input type="number" class="RentrerInfo" name="nbhab" required min="0">
-									</div>
-
-								</div>
-
-								<div class="colonnedroite">
-
-									<div id="ajoutHab2">
-
-										<label for="nbpiece"> Nombre de pièce: </label>
-										<input type="number" class="RentrerInfo" name="nbpiece" required min="0" >
-
-										<label for="addresse"> Addresse: </label>
-										<input type="text" class="RentrerInfo" name="adresse" required minlength="2" size="10">
-
-										<input type="submit" value="Valider" class="envoyer" name="valide1">
-										<input type="submit" value="Annuler" class="envoyer" is="suppr">
-
-
-									</div>
-
-								</div>
-							</form>
-							<br/> <br/>
-						
-							
+							</div>
 
 							<fieldset id="utilSecond">
 								<legend > <ul class="utilisateursecond">
@@ -428,13 +509,13 @@ if (isset($_GET['valide1']))
 
 
 									</table>
-									<input type="submit" value="Valider" class="envoyer" name="valide2">
+									<input type="submit" value="Valider" class="envoyer">
 									<input type="submit" value="Annuler" class="envoyer" is="suppr">
 								</div>
 
-							
 
-							
+
+							</form>
 
 						</fieldset>
 
@@ -444,9 +525,10 @@ if (isset($_GET['valide1']))
 					
 
 						<?php 
+
 						include "footer.php";
+
 						?>
 
 					</body>
-</html>
-	
+					</html>
