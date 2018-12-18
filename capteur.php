1fc7e83890a91e5ce411c7365ee7bb8e7d291428
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-18 10:21:23 +0100

    Update capteur.php

--- capteur.php
+++ capteur.php
@@ -1,107 +1,61 @@
 
-<style>
-	
-
-fieldset {
-	position: relative;
-	margin-left: auto;
-	margin-right: auto;
-	margin-top: 5%;
-	margin-bottom:auto;
-	width: 50em;
-	border:2px solid black;
-	padding: 1%;
-}
-
-
-
-
-input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button {
-   
-   opacity: 1;
-
-}
-
-
-
-#capteur1{
-
-	text-align: left;
-	display: inline-flex;
-	width: 35em;
-	position: relative;
-	margin-left: 0%;
-	margin-top: 5%;
-	padding-bottom: 1%;
-	float: left;
-}
-
-
-
-#type{
-	width: auto;
-	padding-right: 6.5%;
-	padding-left: 0.5%;
-	
-}
-
-
-
-#ajouter{
-	margin-top: 2%;
-	width: 30%;
-	height: 2.5em;
-	position: relative;
-	margin-right: 0%;
-	margin-left: 70%;
-	transition : all 0.3s;
-	font-size: 1.5em;
-
-}
-
-#ajouter:hover{
-
-	background-color: #008CBA; 
-    color: white;
-
-}
-
-
-
-
-</style>
 <?php
 session_start();
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root','');
-$suivant = 0;
-if (isset($_GET['bouton']) AND !empty($_GET['fonctionnalite'])) {
-	$fonctionnalite = $_GET['fonctionnalite'];
-	$resultat= $bdd -> prepare('SELECT reference FROM catalogue WHERE id_type = ?');
-	$resultat -> execute(array($fonctionnalite));
-	$suivant = 1;  }
-
-if (isset($_GET['ajouter']) AND !empty($_GET['reference'])) {
-	$reference=  $_GET['reference'];
-	$nb = $_GET['nombre'];
+if (isset($_POST['reference'])) {
+	$reference=$_POST['reference'];
 	$resultat1 = $bdd -> prepare('SELECT * FROM catalogue WHERE reference = ?');
 	$resultat1 -> execute(array($reference));
 	$capteur = $resultat1 -> fetch(); 
-	$id = $capteur['id_capteur_catalogue'];
-	$insertcapteur = $bdd->prepare("INSERT INTO capteurpiece(id_capteur_catalogue,nbcapteur) VALUES(?, ?)");
-	$insertcapteur-> execute(array($id, $nb));
-	$suivant=1;
-}?>
-
+	$id = $capteur['id_capteur'];
+	$insertcapteur = $bdd->prepare("INSERT INTO capteurpiece(id_capteur_catalogue,id_piece) VALUES(?,?)");
+	$insertcapteur-> execute(array($id,12));
+	header('Location : index.php');
+}
+?>
 <head>
-
-	<meta charset="utf-8">
+	  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+	<meta charset="UTF-8" />
 	<title>Ajouter un capteur</title>
+	<link rel="stylesheet" type="text/css" href="capteur.css" /> 
 </head>
+        <script>
+            function showUser(str)
+            {
+                if (str == "")
+                {
+                    document.getElementById("txtHint").innerHTML = "";
+                    return;
+                }
+                if (window.XMLHttpRequest) {
+                    xmlhttp= new XMLHttpRequest();
+                } else {
+                    if (window.ActiveXObject)
+                        try {
+                            xmlhttp= new ActiveXObject("Msxml2.XMLHTTP");
+                        } catch (e) {
+                            try {
+                                xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+                            } catch (e) {
+                                return NULL;
+                            }
+                        }
+                }
+
+                xmlhttp.onreadystatechange = function ()
+                {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+                    {
+                        document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "getcapteur.php?q=" + str, true);
+                xmlhttp.send();
+            }
+        </script>
 <?php $type= $bdd -> query('SELECT DISTINCT nom_type_capteur,id_type_capteur FROM catalogue JOIN type_capteur ON catalogue.id_type=type_capteur.id_type_capteur');
 
 $req1= $bdd-> prepare('SELECT * FROM piece WHERE id_habitation = ?');
-$req1-> execute(array(1));
-$piece = $req1 ->fetch();
 
 ?>
 
@@ -110,23 +64,19 @@ $piece = $req1 ->fetch();
 			<fieldset>
 				
 			<legend><h2>Ajouter un capteur:</h2></legend>
-			<section>
+
 	
 					<h3>Maison:</h3>
 						<p>Nom de la maison </p>
 
 					<h3>Pièce :</h3>
-						<p><?php echo $piece['nom']?></p>
-					<h3> Superficie :</h3>
-					<p><?php echo $piece['superficie']?> m^2</p>
+						<p>nom de la pièce</p>
 
-				<div id="capteur1">
-					<div id="type">
 						<h3>Capteur:</h3>
 
-							<FORM action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-							<SELECT name="fonctionnalite" size="1" >
-								<option>
+							<FORM>
+							<SELECT name="fonctionnalite" size="1" onchange="showUser(this.value)" >
+								<option value=""> Choisir :</option>
 								<?php 
 								while($donnees=$type->fetch()) 
 								{
@@ -139,43 +89,11 @@ $piece = $req1 ->fetch();
 
 							</SELECT> 	
 					
-						<input type="submit" name="bouton" value="Envoyer">
+						
 					
 					</form>
-					<?php if ($suivant==1) {?>
+					  <div id="txtHint"><b>Les références des capteurs seront notés ici.</b></div>
 
-					
-					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-					</div>
-					<div id="type">
 
-						<h3>Référence :</h3>
-						
 
-							<SELECT name="reference" size="1" >
-								<OPTION >
-								<?php 
-							
-								while($donnees=$resultat->fetch() ) 
-								{
-								?>
-									<option value ="<?=$donnees['reference']?>"><?=$donnees['reference']?></option>
-							    <?php
-							    
-							        }
-							    ?>
-
-							</SELECT> 
-					</div>
-					<div id="nb_capteur">
-						<h3>Nombre de capteurs : </h3>
-						<input type="number" name="nombre" value="0" min="0" max="5" id="nombre">
-						<span class="validity"></span>
-					</div>
-				</div>
-				<input type="submit" name="ajouter" id="ajouter" value="Ajouter">
-
-				</FORM>	
-				</div><?php }?> </fieldset>
-</section>	
 </body>
