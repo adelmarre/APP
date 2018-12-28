b1a1cb02c1939e1826a2d218f648691590bb9d3b
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-28 18:36:02 +0100

    Create affichagecapth.php

---
+++ affichagecapth.php
@@ -0,0 +1,31 @@
+ <?php 
+$hum = $humidite ->fetch();
+for ($i=0 ; $i<$capthexist ; $i++) {?> 
+           
+            <div class="capteur">
+
+            <h3 class="titre"> Humidité </h3>
+
+            <img src="<?php echo $hum['photo']?>" class="avatar_capteur">
+
+            <div class="range">
+              <input type="range" name="eau" min="0" max="100" step="1" class="curseur" list="tickmarks">
+            </div>
+            <datalist id="tickmarks">
+              <option value="0" label="0%">
+                <option value="10">
+                  <option value="20">
+                    <option value="30">
+                      <option value="40">
+                        <option value="50" label="50%">
+                          <option value="60">
+                            <option value="70">
+                              <option value="80">
+                                <option value="90">
+                                  <option value="100" label="100%">
+                                  </datalist>
+
+                                </div>
+
+<?php }?>
+ 
