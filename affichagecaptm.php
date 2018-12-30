20867071ec81f0694557029bf827ead161082005
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-30 11:25:55 +0100

    Update affichagecaptm.php

--- affichagecaptm.php
+++ affichagecaptm.php
@@ -1,16 +1,17 @@
 <?php 
 
-$m = $mouvement ->fetch();
-for ($i=0 ; $i<$captmexist ; $i++) {?> 
+$nb = 1;
+while ($m = $mouvement -> fetch())
+{?> 
 
 	<div class="capteur">
-	<h3 class="titre"> Mouvement </h3>
+	<h3 class="titre"> Mouvement <?php echo $nb?></h3>
 	
 	<img src="<?php echo $m['photo']?>" class="avatar_capteur">
-	<input type="checkbox" name="mouvement" value="<?php echo $m['id_capteur_piece']?>" class="switch">
-		
-
+	<select name="mouvement[]" single>
+	<option  value="<?php echo $m['id_capteur_piece']?>,1" <?php if ( $m['valeur']==1) {echo 'selected="selected"';}?>  class="switch">On </option>
+	<option   value="<?php echo $m['id_capteur_piece']?>,0" <?php if ( $m['valeur']==0) {echo 'selected="selected"';}?>  class="switch">Off</option>
+	</select>
 	</div>
- <?php } ?>
-
-
+ <?php $nb = $nb + 1; } 
+ ?>
