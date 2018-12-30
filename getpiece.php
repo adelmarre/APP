70db91272438b7e1e5c5f30128dc9ef591950905
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-30 11:25:34 +0100

    Update getpiece.php

--- getpiece.php
+++ getpiece.php
@@ -9,61 +9,51 @@ $h = $_GET['h'];
 
 }
 if (isset($_POST['modifier'])) {
-  if (isset($_POST['mouvement'])) {
+
     $c=$_POST['mouvement'];
-    $insertcapteur = $bdd->prepare("UPDATE capteurpiece SET valeur=? WHERE id_capteur_piece= ?");
-    $insertcapteur-> execute(array(1,$c));
-  }
+    $insertcapteur = $bdd->prepare("UPDATE capteurpiece SET valeur=? ");
+    $insertcapteur-> execute(array(2));
+  
 }
 
 ?>
 <head>
     <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="sallestyle.css" /> 
-  <link rel="stylesheet" type="text/css" href="general.css">
+
 </head>
 <?php 
-  $luminosite = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="1"');
-  $temperature = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="2"');
-  $mouvement = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="3"');
-  $fumee = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="4"');
-  $volet = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="5"');
-  $humidite = $bdd -> query('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type_capteur="6"');
+  $luminosite = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $luminosite ->  execute(array(1,$q));
+  $temperature = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $temperature ->  execute(array(2,$q));
+  $mouvement = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $mouvement ->  execute(array(3,$q));
+  $fumee = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $fumee ->  execute(array(4,$q));
+  $volet = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $volet ->  execute(array(5,$q));
+  $humidite = $bdd -> prepare('SELECT * FROM type_capteur JOIN capteurpiece ON type_capteur.id_type_capteur=capteurpiece.id_type WHERE id_type=? AND id_piece=?');
+  $humidite ->  execute(array(6,$q));
  ?>
     <div id="titreSet">
 
-      <form method="post" action traitement.php>
+      <form method="post">
 
         <div class="FDP">
-          <?php 
-          $captlum = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =?'); 
-          $captlum -> execute(array(1,$q));
-          $capttemp = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =?');
-          $capttemp ->  execute(array(2,$q));
-          $captmouv = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =? ');
-          $captmouv ->  execute(array(3,$q));
-          $captfumee = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =?');
-          $captfumee -> execute(array(4,$q));
-          $captvolet = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =?');
-          $captvolet -> execute(array(5,$q));
-          $capthumidite = $bdd -> prepare('SELECT * FROM capteurpiece WHERE id_type=? AND id_piece =?');
-          $capthumidite -> execute(array(6,$q));
-
-          $capthexist = $capthumidite->rowCount();
-          $captlexist = $captlum->rowCount();
-          $capttexist = $capttemp->rowCount();
-          $captmexist = $captmouv->rowCount();
-          $captfexist = $captfumee->rowCount();
-          $captvexist = $captvolet->rowCount();
-       ?><form><?php
-
+          <form><?php
+           echo' <div class="cell">';
           include "affichagecaptl.php";
           include "affichagecaptm.php";
           include "affichagecaptf.php";
+           echo'</div>';
+          echo' <div class="cell">';
+         
           include "affichagecaptv.php";
           include "affichagecapth.php";
           include "affichagecaptt.php"; ?>
-          <input type="submit" id="modifier" value="Activer les modifications" >
+        </div>
+          <input type="submit" id="modifier" name="modifier" value="Activer les modifications" >
 
         </form>
           <div class="ajout">
