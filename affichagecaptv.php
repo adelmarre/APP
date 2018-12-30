2f2674cda3e4dbd20a77dd5bc6cc3c3abc511335
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-30 11:27:20 +0100

    Update affichagecaptv.php

--- affichagecaptv.php
+++ affichagecaptv.php
@@ -1,18 +1,18 @@
 <?php 
-$v = $volet ->fetch();
-for ($i=0 ; $i<$captvexist ; $i++)
-{
- ?>
+$nb = 1;
+while ($v = $volet -> fetch()) {?> 
+ 
   	<div class="capteur">
 
-            <h3 class="titre"> Volet </h3>
-
-            <img src="<?php echo $v['photo']?>" class="avatar_capteur">
-
-            <input type="button" value="↑" name="haut" class="variable" id="monter" onclick="Hautbas()" >
-
-            <input type="button" value="↓" name="bas" class="variable" id="baisser" onclick="Hautbas()">
+            <h3 class="titre"> Volet <?php echo $nb?></h3>
+	<img src="<?php echo $v['photo']?>" class="avatar_capteur">
+	<?php if ( $v['valeur']==0) {?> 
+		<input type="checkbox" name="volet" value="<?php echo $v['id_capteur_piece']?>,1"  class="switch">Descendre </option>
+	<?php }
+	 else {?> 
+		<input type="checkbox"  name="volet" value="<?php echo $v['id_capteur_piece']?>,0"  class="switch">Monter </option>
+	<?php }?>
 
           </div>
-<?php } ?>
+<?php $nb = $nb + 1; } ?>
 
