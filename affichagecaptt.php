b79b57ad20bd9d3163676f8b134c55f35171ae01
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2018-12-31 17:43:52 +0100

    Update affichagecaptt.php

--- affichagecaptt.php
+++ affichagecaptt.php
@@ -9,9 +9,9 @@ while ($t = $temperature -> fetch()) {?>
 
               <img src="<?php echo $t['photo']?>" class="avatar_capteur"  >
 
-
-              <input type="number" name="temperature" step="0.5" min="13" max="30" value="19">
-              <span class="slider round"> </span>
+              <input type="hidden" name="numcapteur" value="<?php echo $t['id_capteur_piece']?>">	
+              <input type="number" name="temperature" step="1" min="0" max="30" value="<?php if ( $t['valeur']!=0) {echo $t['valeur'];}?>">	
+            
             </div>
 <?php $nb = $nb + 1; } ?>
 
