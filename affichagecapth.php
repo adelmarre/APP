82113dab5b96e8ffbe49de3edd54914ec22dc4f4
Clément <45229609+Doubidouchet@users.noreply.github.com>
2019-01-01 15:49:17 +0100

    Update affichagecapth.php

--- affichagecapth.php
+++ affichagecapth.php
@@ -1,4 +1,4 @@
- <?php 
+  <?php 
 $nb = 1;
 while ($hum = $humidite -> fetch()) {?> 
            
@@ -8,24 +8,12 @@ while ($hum = $humidite -> fetch()) {?>
 
             <img src="<?php echo $hum['photo']?>" class="avatar_capteur">
 
-            <div class="range">
-              <input type="range" name="eau" min="0" max="100" step="1" class="curseur" list="tickmarks">
+            <input type="hidden" name="humcapteur" value="<?php echo $hum['id_capteur_piece']?>"> 
+            <label>En pourcentage : </label>
+            <input type="number" name="humidite" step="1" min="30" max="100" value="<?php if ( $hum['valeur']!=0) {echo $hum['valeur'];}?>">
             </div>
-            <datalist id="tickmarks">
-              <option value="0" label="0%">
-                <option value="10">
-                  <option value="20">
-                    <option value="30">
-                      <option value="40">
-                        <option value="50" label="50%">
-                          <option value="60">
-                            <option value="70">
-                              <option value="80">
-                                <option value="90">
-                                  <option value="100" label="100%">
-                                  </datalist>
-
-                                </div>
 
 <?php $nb = $nb + 1; }?>
  
+
+
