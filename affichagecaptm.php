fd92918909fe514d8f797cdafca6e044d1ad48fb
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2019-01-06 16:26:03 +0100

    Update affichagecaptm.php

--- affichagecaptm.php
+++ affichagecaptm.php
@@ -12,6 +12,7 @@ while ($m = $mouvement -> fetch())
 	<option  value="<?php echo $m['id_capteur_piece']?>,1" <?php if ( $m['valeur']==1) {echo 'selected="selected"';}?>  class="switch">On </option>
 	<option   value="<?php echo $m['id_capteur_piece']?>,0" <?php if ( $m['valeur']==0) {echo 'selected="selected"';}?>  class="switch">Off</option>
 	</select>
+	<a href="maisonsallecapteur.php?id_habitation=<?php echo $h?>&amp;supprime=<?php echo $m['id_capteur_piece']?>">Supprimer le capteur</a>	
 	</div>
  <?php $nb = $nb + 1; } 
  ?>
