f15aac36c9185275b5b30c47cfa8a91a427bbea6
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2019-01-06 16:17:10 +0100

    Update header_deco.php

--- header_deco.php
+++ header_deco.php
@@ -1,22 +1,18 @@
-<!DOCTYPE html>
-	<html>	
-	<link rel="stylesheet"  href="header_deco.css">	
+<link rel="stylesheet"  href="header_deco.css">	
 	<meta charset="utf-8">
 		<header>
-				<img src="logo hexagon.png" alt="logo Hexagon" id="logo_hexagon">
+				<img src="image/logo hexagon.png" alt="logo Hexagon" id="logo_hexagon">
 				
 				<div class="deco">
 					<a href="deconnexion.php" class="deconnexion">Déconnexion</a>
 				</div>
 				<nav>
 					<ul>
-						<li><a href="gerermamaison.php">Ma maison</a></li>
+						<li><a href="maisonsallecapteur.php">Ma maison</a></li>
 						<li><a href="catalogue.php">Catalogue</a></li>
 						<li><a href="apropos.php">A Propos</a></li>
-						<li><a href="aide.php">Aide</a></li>
-						<li><a href="">Consignes Globales</a></li>
+						<li><a href="faq.php">Aide</a></li>
+		
 					</ul>
 				</nav>
 			</header>
-
-	</html>
