62d6923736f0520e02dacf6e39214ac003098f52
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2019-01-06 16:26:18 +0100

    Update affichagecaptt.php

--- affichagecaptt.php
+++ affichagecaptt.php
@@ -9,9 +9,10 @@ while ($t = $temperature -> fetch()) {?>
 
               <img src="<?php echo $t['photo']?>" class="avatar_capteur"  >
 
-              <input type="hidden" name="numcapteur" value="<?php echo $t['id_capteur_piece']?>">	
-              <input type="number" name="temperature" step="1" min="0" max="30" value="<?php if ( $t['valeur']!=0) {echo $t['valeur'];}?>">	
-            
+              <input type="hidden" name="<?php echo $nb+10 ?>" value="<?php echo $t['id_capteur_piece']?>"> 
+              <input type="number" name="<?php echo $nb ?>" step="1" min="0" max="30" value="<?php if ( $t['valeur']!=0) {echo $t['valeur'];}?>">
+              <a href="maisonsallecapteur.php?id_habitation=<?php echo $h?>&amp;type=<?php echo $t['id_type']?>&amp;supprime=<?php echo $t['id_capteur_piece']?>">Supprimer le capteur</a>	
+           
             </div>
-<?php $nb = $nb + 1; } ?>
-
+<?php $nb = $nb + 1; }
+ ?>
