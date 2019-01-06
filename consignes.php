d0e29733507c50fd213cb25a01fe775e9c69e92d
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2019-01-06 16:30:23 +0100

    Create consignes.php

---
+++ consignes.php
@@ -0,0 +1,33 @@
+<?php 
+session_start();
+include "menu.php";
+?>
+
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <title>Editer mon profil</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="sallestyle.css" /> 
+  
+</head>
+<body>
+
+
+
+
+<div id="Centrebis">
+
+  <fieldset id="set">
+    <legend> <h1>Consignes globales</h1></legend>
+   Bienvenue sur la page "Consignes globales", vous trouverez ici les règles à respecter et nos conseils dans le cadre de l'offre Domisep. 
+   <ul>
+     <li>Aucun chauffage ne doit être allumé durant les mois Juillet-Août.</li></br>
+     <li>Eteindre vos lumières après votre départ.</li>
+     <li></li>
+   </ul>
+
+</div>  
+  </fieldset>               
+</body>
+
