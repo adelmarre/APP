ccbdba983440519c7314c3fb37aade73f97a8121
loudct <45229675+loudct@users.noreply.github.com>
2019-01-07 17:29:01 +0100

    Add files via upload

--- consignes.php
+++ consignes.php
@@ -22,9 +22,21 @@ include "menu.php";
     <legend> <h1>Consignes globales</h1></legend>
    Bienvenue sur la page "Consignes globales", vous trouverez ici les règles à respecter et nos conseils dans le cadre de l'offre Domisep. 
    <ul>
-     <li>Aucun chauffage ne doit être allumé durant les mois Juillet-Août.</li></br>
-     <li>Eteindre vos lumières après votre départ.</li>
-     <li></li>
+     <br><li>Aucun chauffage ne doit être allumé entre juillet et août.</li></br>
+     <br><li>Eteignez vos lumières et vérifiez que le détecteur de mouvement est activé avant de quitter votre domicile.</li></br>
+     <br><li>Vérifiez la fermeture des volets et de la porte d’entrée avant de partir en vacances.</li></br>
+     <br>
+     <li>Températures idéales : 19°C pour le salon, la salle à manger, la cuisine, le bureau et la salle de bain, entre 16°C et 17°C pour les chambres, 14°C pour les pièces inoccupées. 
+     </li></br>
+     <br><li>En cas d'absence prolongée baisser le chauffage à 14°C. </li></br>
+     <br><li>Taux d’humidité idéale dans une maison : entre 40% et 60%.</li></br>
+     <br><li>Préférez l’utilisation d’ampoules basse consommation pour votre éclairage.</li></br>
+     <br><li>En cas de dysfonctionnement d’une installation contactez nous rapidement. </li></br>
+
+
+
+
+
    </ul>
 
 </div>  
