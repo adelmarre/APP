494a1d33ca22ade993d050e5e90c89c7edfcce4f
Clément <45229609+Doubidouchet@users.noreply.github.com>
2019-01-11 15:41:48 +0100

    Update PageContact.php

--- PageContact.php
+++ PageContact.php
@@ -1,13 +1,120 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=hexagon','root','');
-if(isset($_POST['mailform']))
+if (isset($_SESSION['id'])) {
+	include "header_deco.php";
+	
+	$id = $_SESSION['id'];
+	$user = $bdd -> query("SELECT * FROM personne WHERE id = ".$id."");
+	$userinfo = $user -> fetch();
+	if (isset($_POST['mailform'])){ 
 
-{	
+		if (!empty($_POST['Votre_demande'])) 
+		
+	
+
+	{
+
+		
+    	
+    	$demande = ($_POST['Votre_demande']);
+		$date = date("Y-m-d");
+		$heure = date("H:i:s");
+			$insert = $bdd->prepare('INSERT INTO messageclient(Nom, Prenom,message, heure, ladate, id_destinataire) VALUES(? , ? , ? , ? , ? , ?)');
+		    $insert ->execute(array($userinfo['nom'], $userinfo['prenom'],$demande, $heure , $date ,$id));
+	        $msg = "Demande envoyée! ";               	                            
+	}
+			
+	else
+	{
+		$msg='Le champs doit être complété';
+	}
+}
+
+?>
+<html>
+<link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet"  href="pagecontact.css">
+		<link rel="stylesheet"  href="general.css">
+		<title>Contact</title>
+	</head>
+	<body>
+		
+
+		<div id="bloc_page">
+			<div id="conteneur">
+				<div id="section1">
+						<h1>Nos Coordonées:</h1>
+					<div class="coordonnees">
+						<img src="image/icone_mail.png" alt="icone_mail" id="icone_mail">
+						<div class="mail">projet.hexagon@gmail.com</div>
+						<img src="image/icone_telephone.png" alt="icone_telephone" id="icone_telephone">
+						<div class="telephone">0122334455 </div>
+
+					</div>
+					<?php  
+
+						$reqmessage1 = $bdd -> query('SELECT * FROM messageclient WHERE id_destinataire = "'.$id.'" ');
+						$reqmessage2 = $bdd -> query('SELECT * FROM message WHERE vu=0 AND id_destinataire="'.$id.'" AND nomexpediteur = "admin"');
+						$dejaenvexist = $reqmessage1 ->rowCount();
+      					$pasvuexist = $reqmessage2 ->rowCount(); 
+
+      					if ($dejaenvexist !=0) {?></br ></br >
+					<a href="chat.php?idd=1&amp;ide=<?=$userinfo['id']?>&amp;id=<?=$userinfo['id']?>">Accéder à mon chat</a><p><?php if ($pasvuexist == 0) {echo"";} else {echo "Vous avez $pasvuexist nouveau(x) message(s)";} ?></p></br></br> 
+				<?php } else { ?>
+				</div>
+			</div>
+				<div id="section2">
+					<div id="aside21">
+					<p>Pour nous contacter, merci de bien vouloir remplir le champs ci-dessous. Notre équipe s'engage à vous répondre dans les plus brefs délais</p>
+					</div>
+				
+				<div id="aside22">
+				
+					<div><h2>Votre demande</h2></div>
+					
+					<div class="demande">
+					<ul>
+						<form method="POST" action="">
+							
+							 <div>
+							<textarea type="text" name="Votre_demande" placeholder="Votre message" required > </textarea>
+							<br /> <br />
+							<input type="submit" value="Envoyer" name="mailform">					
+					
+						
+							</div>
+						</form>
+					</div>
+					<?php
+					if (isset($msg)) 
+					{
+						echo '<font color="red">'.$msg."</font>";
+					}
+					?>	
+					
+					</div>
+					<?php } ?> </br ></br ></br ></br ></br ></br ></br ></br ></br >
+				</div>
+
+				<div id="section3">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5438.660570749984!2d2.2743411022013524!3d48.82578830679031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6707980bd3947%3A0xd54fb6c5e1933333!2s10+Rue+de+Vanves%2C+92170+Issy-les-Moulineaux!5e0!3m2!1sfr!2sfr!4v1542127553454" width="480" height="360" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			
+			<?php
+			include "footer.php";
+			?>
+		
+	</body>
+<?php }
+	else {
+		include "header_connexion.php";
 
 
 
-	if( !empty($_POST['Email']) AND !empty($_POST['Prenom']) AND !empty($_POST['Nom']) AND !empty($_POST['Votre_demande']))
+	if (isset($_POST['mailform'])){if( !empty($_POST['Email']) AND !empty($_POST['Prenom']) AND !empty($_POST['Nom']) AND !empty($_POST['Votre_demande']))
 
 	{
 
@@ -33,11 +140,7 @@ if(isset($_POST['mailform']))
 	}
 }
 ?>
-<?php if (isset($_SESSION['id'])) {
-	include "header_deco.php"; }
-	else {
-		include "header_connexion.php";
-	}
+
 ?>
 <html>
 <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
@@ -108,3 +211,7 @@ if(isset($_POST['mailform']))
 			?>
 		
 	</body>
+<?php } ?>	
+
+
+	
