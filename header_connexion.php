4e44f084cabfe6b3cd719ab43c96667d380bfdc5
hashleyjr <45227607+hashleyjr@users.noreply.github.com>
2019-01-23 12:10:09 +0100

    Update header_connexion.php

--- header_connexion.php
+++ header_connexion.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet"  href="header_connexion.css">	
 		<header>
 				<img src="image/logo hexagon.png" alt="logo Hexagon" id="logo_hexagon">
-				
+				<meta charset="utf-8">
 				<div class="co">
 					<a href="index.php" class="connexion">Connexion</a>
 				</div>
 				<nav>
-					<ul>
+					<ul class="snip1241">
 						<li><a href="index.php">Ma maison</a></li>
 						<li><a href="catalogue.php">Catalogue</a></li>
 						<li><a href="apropos.php">A Propos</a></li>
@@ -17,4 +17,3 @@
 				
 			</header>
 
-</html>
